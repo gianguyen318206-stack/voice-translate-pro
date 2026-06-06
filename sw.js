d12947c15fedb33fc518ba1d7e194d7bc57672fd
gianguyen318206-stack <gianguyen318206@gmail.com>
2026-06-06 22:57:20 +0700

    v11: Self-destruct sw.js to clear all browser caches and unregister service workers on client devices

--- sw.js
+++ sw.js
@@ -1,32 +1,17 @@
-const CACHE_NAME = 'vt-pro-v5';
-const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
-
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
     e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-        )
-    );
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-    // Network-First strategy: always try the network first to guarantee fresh code, fallback to cache if offline
-    e.respondWith(
-        fetch(e.request)
-            .then(res => {
-                // If successful, clone response and update cache
-                if (res.status === 200) {
-                    const resClone = res.clone();
-                    caches.open(CACHE_NAME).then(c => c.put(e.request, resClone));
-                }
-                return res;
+        caches.keys()
+            .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+            .then(() => self.registration.unregister())
+            .then(() => self.clients.matchAll())
+            .then(clients => {
+                clients.forEach(client => {
+                    try { client.navigate(client.url); } catch(err) {}
+                });
             })
-            .catch(() => caches.match(e.request))
     );
 });
